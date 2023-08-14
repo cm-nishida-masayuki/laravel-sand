c8dc522b353f865cb9f5509f86974faf538c7faa
cm-nishida-masayuki <nishida.masayuki@classmethod.jp>
2023-08-15 07:53:31 +0900

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,26 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Install Composer
-EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+echo "build"
 
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
-    rm composer-setup.php
-    exit 1
-fi
-
-php composer-setup.php
-rm composer-setup.php
-
-# Install dependencies
-
+# Laravel の動作に必要なソフトウェアをインストール
 composer --version
-yum install php-mbstring php-xml wget tar -y
+yum install php-mbstring php-xml -y
 amazon-linux-extras install nginx1 -y
-php composer.phar install
+composer install
 cp -p .env.example .env
 php artisan key:generate
 
@@ -33,11 +19,22 @@ chown -R :nginx ./storage
 chown -R :nginx ./bootstrap/cache
 chown -R :nginx ./public
 
-chmod -R 777 ./storage
+find ./storage -type d -exec chmod 775 {} \;
+find ./storage -type f -exec chmod 664 {} \;
 
 find ./bootstrap/cache -type d -exec chmod 775 {} \;
 find ./bootstrap/cache -type f -exec chmod 664 {} \;
 
+find ./storage -type d -exec chmod g+s {} \;
 find ./bootstrap/cache -type d -exec chmod g+s {} \;
 
+setfacl -R -d -m g::rwx ./storage
 setfacl -R -d -m g::rwx ./bootstrap/cache
+
+# アプリケーションのログ出力先を設定
+ln -s /dev/stdout /var/log/nginx/error.log
+ln -s /dev/stdout /var/log/nginx/access.log
+
+ln -s /dev/stdout /var/log/php-fpm/error.log
+ln -s /dev/stdout /var/log/php-fpm/www-access.log
+ln -s /dev/stdout /var/log/php-fpm/www-error.log
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-php artisan config:clear
-
-set -o monitor
-
-trap exit SIGCHLD
-
-# Start nginx 
-nginx -g 'daemon off;' &
-
-# Start php-fpm
-php-fpm -F &
-
-wait
+php-fpm -D
+/usr/sbin/nginx -g "daemon off;"
